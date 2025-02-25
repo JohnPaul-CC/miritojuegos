b7004689d7ccc7d19b642c1b61fecfe69d867428
JohnPaul-CC <jhon.p.cc2003@gmail.com>
2025-02-25 22:57:00 +0100

    Mergeo

--- app/src/main/java/com/example/appjohn/MainActivity.kt
+++ app/src/main/java/com/example/appjohn/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
